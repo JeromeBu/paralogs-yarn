9d28de42ec1077ebd7aee06bd5ee729efd38c425
Jerome Burkard <jerome.burkard@gadz.org>
2019-12-11 11:34:39 +0100

    add concurently and simplify dev scripts

--- packages/front/src/ui/components/views/Home.tsx
+++ packages/front/src/ui/components/views/Home.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-import { salut } from "@paralogs/shared";
 import { MyLink } from "../commun/MyLink";
 
 export const Home: React.FC = () => {
   return (
     <p>
       Welcome, got to <MyLink to="flights">Flights</MyLink>
-      <br />
-      Hey : {salut}
     </p>
   );
 };
