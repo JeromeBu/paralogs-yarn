6ba7d3216dba91c60a9efc6cb4e349859619f332
Jerome Burkard <jerome.burkard@gadz.org>
2019-12-11 11:48:43 +0100

    change createWingUseCase from class to function

--- packages/back/src/domain/useCases/CreateWingUseCase.ts
+++ packages/back/src/domain/useCases/CreateWingUseCase.ts
@@ -2,17 +2,13 @@ import { Wing } from "@paralogs/shared";
 import { WingRepo } from "../port/WingRepo";
 import { notUnique } from "../core/errors";
 
-export class CreateWingUseCase {
-  private wingRepo: WingRepo;
-
-  constructor(wingRepo: WingRepo) {
-    this.wingRepo = wingRepo;
-  }
-
-  public async execute(wing: Wing) {
-    const existingWing = await this.wingRepo.findById(wing.id);
+export const createWingUseCaseCreator = (wingRepo: WingRepo) => {
+  return async (wing: Wing) => {
+    const existingWing = await wingRepo.findById(wing.id);
     if (existingWing) throw notUnique("Wing");
-    await this.wingRepo.save(wing);
+    await wingRepo.save(wing);
     return wing;
-  }
-}
+  };
+};
+
+export type CreateWingUseCase = ReturnType<typeof createWingUseCaseCreator>;
