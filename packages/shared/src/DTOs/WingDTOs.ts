8c76340a72408a42c143d2a4574f28f833c34d7f
Jerome Burkard <jerome.burkard@gadz.org>
2020-01-29 09:49:29 +0100

    sharing yup schemas for validations

--- packages/shared/src/DTOs/WingDTOs.ts
+++ packages/shared/src/DTOs/WingDTOs.ts
@@ -1,3 +1,4 @@
+import * as Yup from "yup";
 import { DateString, NumberOfMinutes } from "../generalTypes/types";
 
 export interface CreateWingDTO {
@@ -8,6 +9,16 @@ export interface CreateWingDTO {
   ownerUntil?: DateString;
   flightTimePriorToOwn: NumberOfMinutes;
 }
+
+export const createWingSchema = Yup.object().shape<CreateWingDTO>({
+  id: Yup.string().required(),
+  brand: Yup.string().required(),
+  model: Yup.string().required(),
+  ownerFrom: Yup.string().required(),
+  ownerUntil: Yup.string(),
+  flightTimePriorToOwn: Yup.number(),
+});
+
 export interface WingDTO extends CreateWingDTO {
   userId: string;
 }
