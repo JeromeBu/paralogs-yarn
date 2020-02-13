d0e95b69aa53495b4929b475b414fa867ee11eb5
Jerome Burkard <jerome.burkard@gadz.org>
2020-02-13 23:27:00 +0100

    create http calls with token from front

--- packages/front/src/core-logic/adapters/libs/httpClient.ts
+++ packages/front/src/core-logic/adapters/libs/httpClient.ts
@@ -12,45 +12,43 @@ import { map } from "rxjs/internal/operators/map";
 import { Observable } from "rxjs/internal/Observable";
 import { config } from "../../config";
 import { LocalClientStorage } from "../LocalClientStorage";
-import { ClientStorage } from "../../useCases/auth/port/ClientStorage";
 
 const responseToObservable = <Output>(
   axiosResponsePromise: Promise<AxiosResponse<Output>>,
 ) => from(axiosResponsePromise).pipe(map(({ data }) => data));
 
-const createPostRequest = <Input, Output>(
-  route: string,
-  axiosConfig?: AxiosRequestConfig,
-) => (input: Input): Observable<Output> =>
+const POST = <Input, Output>(route: string, axiosConfig?: AxiosRequestConfig) => (
+  input: Input,
+): Observable<Output> =>
   responseToObservable(
     axios.post<Output>(`${config.apiUrl}${route}`, input, axiosConfig),
   );
 
-const createGetRequest = <Output>(
-  route: string,
-  axiosConfig?: AxiosRequestConfig,
-) => (): Observable<Output> =>
-  responseToObservable(axios.get(`${config.apiUrl}${route}`, axiosConfig));
+const GET = <Output>(route: string, axiosConfig?: AxiosRequestConfig) => (): Observable<
+  Output
+> => responseToObservable(axios.get(`${config.apiUrl}${route}`, axiosConfig));
 
 const localClientStorage = new LocalClientStorage();
 
-const withAuthTokenCreator = (clientStorage: ClientStorage) => <Output>(
-  requestFunction: typeof createGetRequest,
-  route: string,
-) =>
-  requestFunction<Output>(route, {
+const GETwithToken = <Output>(route: string) =>
+  GET<Output>(route, {
     headers: {
-      authorization: `Bearer ${clientStorage.get("token")}`,
+      authorization: `Bearer ${localClientStorage.get("token")}`,
     },
   });
 
-const withAuthToken = withAuthTokenCreator(localClientStorage);
+const POSTwithToken = <Input, Output>(route: string) =>
+  POST<Input, Output>(route, {
+    headers: {
+      authorization: `Bearer ${localClientStorage.get("token")}`,
+    },
+  });
 
 export const httpClient = {
-  signUp: createPostRequest<SignUpParams, CurrentUserWithAuthToken>("users/signup"),
-  logIn: createPostRequest<LoginParams, CurrentUserWithAuthToken>("users/login"),
-  logout: createGetRequest("users/logout"),
-  retrieveUsers: createGetRequest<UserDTO[]>("users"),
-  retrieveWings: withAuthToken<WingDTO[]>(createGetRequest, "wings"),
-  addWing: createPostRequest<CreateWingDTO, WingDTO>("wings"),
+  signUp: POST<SignUpParams, CurrentUserWithAuthToken>("users/signup"),
+  logIn: POST<LoginParams, CurrentUserWithAuthToken>("users/login"),
+  logout: GETwithToken("users/logout"),
+  retrieveUsers: GET<UserDTO[]>("users"),
+  retrieveWings: GETwithToken<WingDTO[]>("wings"),
+  addWing: POSTwithToken<CreateWingDTO, WingDTO>("wings"),
 };
