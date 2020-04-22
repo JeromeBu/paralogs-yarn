31697a8ecc58a72526768d17bb0874487b2d6b32
Jerome Burkard <jerome.burkard@gadz.org>
2020-04-22 15:53:19 +0200

    fix routes called with double slash

--- packages/front-core/src/config.ts
+++ packages/front-core/src/config.ts
@@ -8,15 +8,15 @@ const buildConfig = () => {
   switch (environment) {
     case "production":
       return {
-        apiUrl: "https://s8splmzrh5.execute-api.eu-west-1.amazonaws.com/production/",
+        apiUrl: "https://s8splmzrh5.execute-api.eu-west-1.amazonaws.com/production",
       };
     case "staging":
       return {
-        apiUrl: "https://r3su0zwym6.execute-api.eu-west-1.amazonaws.com/staging/",
+        apiUrl: "https://r3su0zwym6.execute-api.eu-west-1.amazonaws.com/staging",
       };
     default:
       return {
-        apiUrl: "http://localhost:4000/",
+        apiUrl: "http://localhost:4000",
       };
   }
 };
