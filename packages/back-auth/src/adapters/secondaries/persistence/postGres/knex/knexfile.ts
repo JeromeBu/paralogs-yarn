39ffc4e2496cce69ad5e21e33229ff3a0da6b294
Jerome Burkard <jerome.burkard@gadz.org>
2020-05-27 15:14:20 +0200

    share the same database between BC

--- packages/back-auth/src/adapters/secondaries/persistence/postGres/knex/knexfile.ts
+++ packages/back-auth/src/adapters/secondaries/persistence/postGres/knex/knexfile.ts
@@ -1,41 +1,39 @@
 import { EnvironmentOption } from "../../../../../config/env";
 
+const migrations = {
+  // schemaName: "auth",
+  tableName: "knex_auth_migrations",
+  extension: "ts",
+  directory: `${__dirname}/migrations`,
+};
+
+const pool = {
+  min: 2,
+  max: 10,
+};
+
 const knexconfig: { [key in EnvironmentOption]: any } = {
   development: {
     client: "pg",
     connection: {
-      port: 5442,
-      database: "paralogs-auth-dev",
+      port: 5432,
+      database: "paralogs-dev",
       user: "postgres",
       password: "pg-password",
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      extension: "ts",
-      directory: `${__dirname}/migrations`,
-    },
+    pool,
+    migrations,
   },
   test: {
     client: "pg",
     connection: {
-      port: 5443,
-      database: "paralogs-auth-test",
+      port: 5433,
+      database: "paralogs-test",
       user: "postgres",
       password: "pg-password",
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      extension: "ts",
-      directory: `${__dirname}/migrations`,
-    },
+    pool,
+    migrations,
   },
 
   // production: {
